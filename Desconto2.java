0a9dd8b9a48c21274cc68c3e9e7d3619fc0f90d0
melvinsatiro <melvinsatiro@outlook.com>
2025-08-18 20:20:12 -0300

    Adiciona a classe Desconto2

---
+++ Desconto2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Desconto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Digite o valor da compra");
+        Scanner scanner = new Scanner(System.in);
+
+        double valor = scanner.nextInt();
+
+        if(valor >= 100){
+
+         double desconto = 0.1;
+         double numero = valor * desconto;
+         double total = valor - numero;
+
+            System.out.println("Com Desconto fica:"+ total);
+        }else if( valor < 100){
+
+            System.out.println("Sem Desconto  = R$ "+ valor);
+        }
+    }
+
+}
